41ba25dbaef6bb167b87e1321b69e6b2af6719b3
alchemist1111 <emoyoeugine@gmail.com>
2024-07-02 08:24:20 +0300

    updates

--- server/models.py
+++ server/models.py
@@ -1,9 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+metadata = MetaData()
+db = SQLAlchemy(metadata=metadata)
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Plant {self.id}, {self.name}, {self.image}, {self.price}>'
